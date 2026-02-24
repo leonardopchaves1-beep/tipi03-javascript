81733dc9b798c651c6b666bd1a9a087be8244682
LEONARDO PINTO CHAVES <leonardo.pchaves@senacsp.edu.br>
2026-02-24 11:37:45 -0300

    Aula 24-02

---
+++ 02-concatenacaointerpolacao/script.js
@@ -0,0 +1,17 @@
+//Concatenação
+const meutexto = "Óla" + "Mundo";
+console.log(meutexto);
+
+const numeronovo = 15;
+console.log("O número é " + numeronovo);
+
+const meutexto2 = "Óla" .concat("mundo novamente");
+console.log(meutexto2);
+
+//Interpolando uma string, um número e uma expressão
+const primeironome = "João";
+const idade = 30;
+console.log(`Olá, ${primeironome}, você tem${idade} anos`);
+
+//comparação
+console.log("Óla, " + primeironome + " você tem " + idade + " anos.")
