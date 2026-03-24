ad9322ce1de5d701fa94b3ccec2f74485da1e65f
LEONARDO PINTO CHAVES <leonardo.pchaves@senacsp.edu.br>
2026-03-24 11:59:20 -0300

    subindo arquivos novos

---
+++ 19-programacaoassincrona/js/script.js
@@ -0,0 +1,32 @@
+//setTimeOut - só exexuta uma vez
+function exibirMensagem(){
+    console.log('A mensagem foi exibida após 6 segundos')
+}
+ 
+setTimeout(exibirMensagem, 6000);
+
+// SetInterval
+/*
+function exebirMensagem (){
+console.log('A Mensagem foi exibida após 3 segundos');
+}
+
+setInterval(exibirMensagem, 3000);
+*/
+
+
+//async function
+async function esperar(segundos){
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`Esperei ${segundos} segundos.`);
+            }, segundos * 1000);
+        });
+    }
+
+
+    async function executar(){
+        const resultado = await esperar (3);
+        console.log(resultado);
+    }
+    executar();
